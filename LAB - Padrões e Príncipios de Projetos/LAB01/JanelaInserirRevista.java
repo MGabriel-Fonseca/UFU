402b11c633b2736b8e0951a038b2c49401011f3a
Marcos Gabriel Moreira Fonseca <gabriel.kconde@ufu.br>
2025-02-17 17:23:51 -0300

    ppp

--- LAB - Padrões e Príncipios de Projetos/LAB01/JanelaInserirRevista.java
+++ LAB - Padrões e Príncipios de Projetos/LAB01/JanelaInserirRevista.java
@@ -6,45 +6,54 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class JanelaInserirRevista extends JFrame {
-    private JTextField campoTitulo, campoAutor, campoEdicao;
+    private JTextField campoTitulo, campoOrg, campoVol, campoNro, campoAno;
     private JButton botaoIncluir, botaoLivros, botaoListagem;
 
     public JanelaInserirRevista() {
         setTitle("Inserir Revista");
-        setSize(300, 250);
+        setSize(300, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
 
         campoTitulo = new JTextField(20);
-        campoAutor = new JTextField(20);
-        campoEdicao = new JTextField(20);
+        campoOrg = new JTextField(20);
+        campoVol = new JTextField(5);
+        campoNro = new JTextField(5);
+        campoAno = new JTextField(5);
         botaoIncluir = new JButton("Incluir Revista");
         botaoLivros = new JButton("Livros");
         botaoListagem = new JButton("Listagem");
 
         add(new JLabel("Título:"));
         add(campoTitulo);
-        add(new JLabel("Autor:"));
-        add(campoAutor);
-        add(new JLabel("Edição:"));
-        add(campoEdicao);
+        add(new JLabel("Org:"));
+        add(campoOrg);
+        add(new JLabel("Volume:"));
+        add(campoVol);
+        add(new JLabel("Número:"));
+        add(campoNro);
+        add(new JLabel("Ano:"));
+        add(campoAno);
         add(botaoIncluir);
         add(botaoLivros);
         add(botaoListagem);
-
 
         botaoIncluir.addActionListener(e -> {
             try {
                 MaterialBibliografico.adicionarMaterialBibliografico(
                         new Revista(campoTitulo.getText(),
-                                campoAutor.getText(),
-                                Integer.parseInt(campoEdicao.getText()))
+                                campoOrg.getText(),
+                                Integer.parseInt(campoVol.getText()),
+                                Integer.parseInt(campoNro.getText()),
+                                Integer.parseInt(campoAno.getText()))
                 );
                 JOptionPane.showMessageDialog(null, "Revista incluída.");
 
                 campoTitulo.setText("");
-                campoAutor.setText("");
-                campoEdicao.setText("");
+                campoOrg.setText("");
+                campoVol.setText("");
+                campoNro.setText("");
+                campoAno.setText("");
 
             } catch (NumberFormatException error) {
                 JOptionPane.showMessageDialog(null, "Não foi possível incluir esta revista.");
@@ -53,7 +62,7 @@ public class JanelaInserirRevista extends JFrame {
 
         botaoLivros.addActionListener(e -> {
             new JanelaInserirLivro();
-            this.setVisible(false); 
+            this.setVisible(false);
         });
 
         botaoListagem.addActionListener(e -> {
--- LAB - Padrões e Príncipios de Projetos/LAB01/Revista.java
+++ LAB - Padrões e Príncipios de Projetos/LAB01/Revista.java
@@ -1,17 +1,47 @@
 public class Revista extends MaterialBibliografico {
-    private int edicao;
+    private String org;   
+    private int vol;     
+    private int nro;     
+    private int ano;    
 
-    public Revista(String titulo, String autor, int edicao) {
-        super(titulo, autor);
-        this.edicao = edicao;
+    public Revista(String titulo, String org, int vol, int nro, int ano) {
+        super(titulo, null); 
+        this.org = org;
+        this.vol = vol;
+        this.nro = nro;
+        this.ano = ano;
     }
 
-    public int getEdicao() {
-        return edicao;
+    public String getOrg() {
+        return org;
     }
 
-    public void setEdicao(int edicao) {
-        this.edicao = edicao;
+    public void setOrg(String org) {
+        this.org = org;
+    }
+
+    public int getVol() {
+        return vol;
+    }
+
+    public void setVol(int vol) {
+        this.vol = vol;
+    }
+
+    public int getNro() {
+        return nro;
+    }
+
+    public void setNro(int nro) {
+        this.nro = nro;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
     }
 
     @Override
@@ -21,6 +51,6 @@ public class Revista extends MaterialBibliografico {
 
     @Override
     public String toString() {
-        return "Revista: " + getTitulo() + " | Autor: " + getAutor() + " | Edição: " + edicao;
+        return "Revista: " + getTitulo() + " | Org: " + org + " | Vol: " + vol + " | Nro: " + nro + " | Ano: " + ano;
     }
 }
